86a6c86b1e0e2eee814c930a62d0342eac13eade
Kamrin Hurley <128550647+KamH333@users.noreply.github.com>
2023-10-25 08:52:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './emissionSentriLogo.png';
+import icon from './show-password.png';
+import React, { useState } from 'react';
 
 function App() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+
+  }
+  const handleLogin = () => {
+    setEmailError(email ? '' : 'email is required');
+    setPasswordError(password ? '' : 'password is required');
+  
+    if (email && password) {
+      setErrorMessage(''); // Clear any previous error message
+      // Perform the login logic here
+      return 'Login successful';
+    }
+  };
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div id="container">
+      <div id="form">
+        <img id="logo" src={logo} alt="Emission Sentri Logo"></img>
+        <form id="formItems">
+          <div className="input-container">
+             <label htmlFor="email">Email</label> 
+             <br></br>
+             <input 
+              id="email" 
+              type="email" 
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{ border: emailError ? '1px solid red' : '1px solid #ccc' }}>
+            </input>
+            {emailError && (
+              <div className= "error-bubble">
+                This field cannot be empty
+              </div>
+            )}
+          </div>
+          <div id="second">
+          <br></br>
+          <label htmlFor="password">Password</label>
+          <a><strong>Reset password</strong></a>
+          <br></br>
+          <div className="password-input">
+          <input 
+            id="password" 
+            type= {showPassword ? 'text' : 'password'}
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ border: passwordError ? '1px solid red' : '1px solid #ccc' }}>
+          </input>
+          {passwordError && (
+              <div className= "error-bubble">
+                This field cannot be empty
+              </div>
+          )}
+          <button 
+          id="showIcon"
+          onClick={(e) => {
+            e.preventDefault();
+            togglePasswordVisibility();
+          }}
+          >
+            <img src={icon} alt="show password"></img>
+            </button>
+          </div>
+        </div>
+        </form>
+        <div className="error-message">{errorMessage}</div>
+        <button onClick={handleLogin}><strong>Log in</strong></button>
+        </div>
+      </div>
   );
 }
-
 export default App;
